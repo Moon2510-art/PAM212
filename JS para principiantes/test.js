e846f9bef41b5d254b89174ad51ead1cf023cf24
Madrigal <improbable2510@gmail.com>
2025-09-10 23:20:26 -0600

    Uso de operador if y switch en JS

--- JS para principiantes/test.js
+++ JS para principiantes/test.js
@@ -1,22 +1,36 @@
-let numberOne = 60;
-let numberTwo = 100;
-
-let result = numberOne + numberTwo;
-let comparison = numberOne < numberTwo;
-
-console.log(comparison);
-console.log(result);
-
-let name = "John";
-let lastName = "Carter";
-
-let fullName = name + " " + lastName;
-
-console.log(fullName);
-
 let passwordDB = "root";
-let input = "notroot"
+
+let input = "notroot";
 
 let output = input == passwordDB;
 
+if (output == true){
+    console.log("Login succesful!");
+} else {
+    console.log("Wrong Password!");
+}
+
 console.log(output);
+
+let score = 70;
+
+if (score > 30) {
+    console.log("You need more practice");
+} else if (score > 15) {
+    console.log("You're getting better at this");
+} else {
+    console.log("You need to follow this tutorial");
+}
+
+let typecard = "Debit Card";
+
+switch(typecard) {
+    case "Debit Card":
+        console.log("This is a debit card");
+        break;
+    case "Credit Card":
+        console.log("This is a credit card");
+        break;
+    default:
+        console.log("No card");
+}
