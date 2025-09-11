8d17d6b2d6c09baa64158d7bc24a838010d58e88
Madrigal <improbable2510@gmail.com>
2025-09-10 23:31:23 -0600

    Ejemplos de usos de operadores y strings

--- JS para principiantes/test.js
+++ JS para principiantes/test.js
@@ -1,36 +1,14 @@
-let passwordDB = "root";
+let count = 50;
 
-let input = "notroot";
-
-let output = input == passwordDB;
-
-if (output == true){
-    console.log("Login succesful!");
-} else {
-    console.log("Wrong Password!");
+while(count > 0) {
+    console.log(count);
+    count = count - 1;
 }
 
-console.log(output);
+let names = ["ryan", "joe", "john", "mario"];
+console.log(names[3]);
+console.log(names.length);
 
-let score = 70;
-
-if (score > 30) {
-    console.log("You need more practice");
-} else if (score > 15) {
-    console.log("You're getting better at this");
-} else {
-    console.log("You need to follow this tutorial");
-}
-
-let typecard = "Debit Card";
-
-switch(typecard) {
-    case "Debit Card":
-        console.log("This is a debit card");
-        break;
-    case "Credit Card":
-        console.log("This is a credit card");
-        break;
-    default:
-        console.log("No card");
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
 }
