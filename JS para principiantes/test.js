6a2a7aff0450fef3f2f5cac85f56099f2fa2feee
Madrigal <improbable2510@gmail.com>
2025-09-10 22:41:55 -0600

    Uso de funciones de variables en JS

--- JS para principiantes/test.js
+++ JS para principiantes/test.js
@@ -1,2 +1,11 @@
 document.write("<h1>hello world<h1>");
 console.log({"username": "ryan", "score": 70.4})
+
+var nameuser = "John";
+let lastname = "Carter";
+
+nameuser = 'Pepe';
+const pi = "3.1416";
+const id = "User1"
+
+console.log(lastname)
