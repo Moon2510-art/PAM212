85878ab752669a65f82dd25c4f0e95464967ad9b
Madrigal <improbable2510@gmail.com>
2025-09-18 18:44:13 -0600

    Codigo A corregido.

---
+++ Practica1/a.js
@@ -0,0 +1,10 @@
+
+let nombre = "Armando";
+let edad = 25;
+
+nombre = "Ana Maria";
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+//muestra en consola en contenido de saludo
+
+console.log(saludo)
