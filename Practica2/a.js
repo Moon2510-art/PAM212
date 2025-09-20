30f309704e6a418785a4e1990ac0fafa244353fc
Madrigal <improbable2510@gmail.com>
2025-09-19 18:34:35 -0600

    Ejercicio A Practica 2

---
+++ Practica2/a.js
@@ -0,0 +1,12 @@
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37,
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+    }
+};
+
+const {nombre, edad, direccion} = persona;
+
+console.log("Me llamo " + nombre + ", tengo " + edad + " y vivo en " + direccion.ciudad);
