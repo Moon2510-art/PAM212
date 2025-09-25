8e40229f5d3313a3d0acfb4a24c71f6dffad2095
Madrigal <improbable2510@gmail.com>
2025-09-25 17:55:57 -0600

    Pruebas para import de substractnums

--- Practica3/Main.js
+++ Practica3/Main.js
@@ -1,3 +1,6 @@
 import { SubstractNums } from "Utils.js";
 
 console.log(SubstractNums(5, 2));
+console.log(SubstractNums(3, 1));
+console.log(SubstractNums(1, 1));
+console.log(SubstractNums(10, 9));
