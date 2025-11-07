7c5c5c7cdb17fbdc37818eab699dae5938ea07d2
Madrigal <improbable2510@gmail.com>
2025-11-07 14:25:54 -0600

    Practica bottom sheet test

--- intro/screens/ModalScreen.js
+++ intro/screens/ModalScreen.js
@@ -1,14 +1,72 @@
-import { Text, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react';
+import { Modal, View, Text, Button, StyleSheet, } from 'react-native';
 
-export default function ModalScreen() 
-{
+export default function App() {
+  const [modalVisible, setModalVisible] = useState(false); // STATE: controla si modal está abierto
 
-    return (
-      <View>
-        <Text>Modalscreen Proximamente...</Text>
-      </View>
-    )
-  }
+  const abrirModal = () => {
+    setModalVisible(true);
+  };
 
+  const cerrarModal = () => {
+    setModalVisible(false);
+  };
 
-const styles = StyleSheet.create({})
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ejemplo del componente Modal</Text>
+
+      <Button title="Abrir Modal" onPress={abrirModal} /> 
+
+      <Modal
+        animationType="fade"            // PROP: tipo de animación al abrir slide, fade, none
+        transparent={true}               // PROP: fondo semitransparente
+        visible={modalVisible}           // PROP: controla visibilidad (STATE)
+        onRequestClose={cerrarModal}     // PROP: acción al cerrar (Android)
+      >
+
+        <View style={styles.modalContainer}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>¡Hola! Este es un Modal.</Text>
+            <Button title="Cerrar" onPress={cerrarModal} /> {/* PROP onPress: cierra modal */}
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000ff',
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.4)',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 15,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    fontSize: 16,
+  },
+});
